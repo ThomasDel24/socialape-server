92449fb6a25500f95006d975bcaa497e8e726e34
Thomas DelMastro <tdelmastro24@gmail.com>
2021-04-20 08:32:58 -0700

    First Commit

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyDGEkpY2LJqbLT1qcXK0XxQNwS-Td_0OI4",
+    authDomain: "social-web-909c6.firebaseapp.com",
+    projectId: "social-web-909c6",
+    storageBucket: "social-web-909c6.appspot.com",
+    messagingSenderId: "865857345314",
+    appId: "1:865857345314:web:d36cc9bef48f8389ac777b",
+    measurementId: "G-ERWD921N03"
+};
